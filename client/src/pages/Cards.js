e42dc3020914338b8119d367b4f4bbb3cd01e69e
Ariell SCott <scott.ariell00@gmail.com>
2021-10-25 14:06:57 -0400

    add cards.js to pages to display goals by topic

---
+++ client/src/pages/Cards.js
@@ -0,0 +1,17 @@
+// export default Profile;
+import React from "react";
+
+// reactstrap components
+import {
+  Button,
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  CardText,
+  FormGroup,
+  Form,
+  Input,
+  Row,
+  Col,
+} from "reactstrap";
