67ba5b23621db020c5e7e30300d02a05bf5dd063
Ariell SCott <scott.ariell00@gmail.com>
2021-10-26 14:52:54 -0400

    set dummy seeds to remove error

--- server/models/Goals.js
+++ server/models/Goals.js
@@ -2,29 +2,22 @@ const { Schema, model } = require('mongoose');
 const bcrypt = require('bcrypt');
 
 const goalsSchema = new Schema({
-    start: {
-        type: Date,
-        default: Date.prototype.setDate,
-        required: true,
-    },
-    end: {
-        type: Date,
-        required: true
-    },
-    exercise: [
-        {
-        type: {
-            type: String,
-            required: true,
-        }
-        
-        
+    // start: {
+    //     type: Date,
+    //     default: Date.prototype.setDate,
+    //     required: true,
+    // },
+    // end: {
+    //     type: Date,
+    //     required: true
+    // },
+    exercise: {
+        type: String
     }
-]
 
  
 });
 
-const Goals = model('Goals', goalSchema);
+const Goals = model('Goals', goalsSchema);
 
 module.exports = Goals;
