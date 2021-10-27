f7cc4c689113b92c8af07adfb7e271670d7e9ad5
Lajaynees <lscingram@yahoo.com>
2021-10-27 12:29:17 -0400

    updated href to Link to=

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -3,10 +3,10 @@ import { Link } from 'react-router-dom';
 import "./index.css";
 import Auth from '../../utils/auth';
 // import Button from 'react-bootstrap/Button';
-import BootstrapNavbar from 'react-bootstrap/Navbar';
+// import BootstrapNavbar from 'react-bootstrap/Navbar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-const Navbar = () =>  {
+const Navbar = () => {
   const logout = (event) => {
     event.preventDefault();
     Auth.logout();
@@ -24,45 +24,43 @@ const Navbar = () =>  {
       </>
     );
   }
-//If logged out show login controls
 
   return (
     <>
-     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">L.Y.B.L</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
+     <nav className="navbar navbar-expand-lg navbar-light bg-light">
+  <Link className="navbar-brand" to="/">L.Y.B.L</Link>
+  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span className="navbar-toggler-icon"></span>
   </button>
 
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav ml-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Dashboard <span class="sr-only">(current)</span></a>
+  <div className="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul className="navbar-nav ml-auto">
+      <li className="nav-item active">
+        <Link className="nav-link" to="/">Dashboard <span className="sr-only">(current)</span></Link>
       </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Log-Out</a>
+      <li className="nav-item">
+        <Link className="nav-link" to="/">Log-Out</Link>
       </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+      <li className="nav-item dropdown">
+        <Link className="nav-link dropdown-toggle" to="/" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
           Dropdown
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">Profile</a>
-          <a class="dropdown-item" href="#">Goals</a>
-          {/* <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Something else here</a> */}
+        </Link>
+        <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+          <Link className="dropdown-item" to="/">Profile</Link>
+          <Link className="dropdown-item" to="/">Goals</Link>
+          {/* <div className="dropdown-divider"></div>
+          <a className="dropdown-item" href="#">Something else here</a> */}
         </div>
       </li>
     </ul>
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"></input>
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    <form className="form-inline my-2 my-lg-0">
+      <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"></input>
+      <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
     </form>
   </div>
 </nav> 
-</>
-      )
-
+    </>
+  );
 };
 
-      export default Navbar;
+export default Navbar;
