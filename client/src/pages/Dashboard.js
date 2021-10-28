c86a91a80a2dc2f4e7341629418086f9c18c2cb6
Mel Vint <melvint@Mels-MacBook-Pro.local>
2021-10-27 23:10:38 -0400

    created dashboard layout

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,11 +1,95 @@
-import React from 'react'
+import React from "react";
+import { Col, Row, Container } from "reactstrap";
+import Dashboard from "pages";
 
 function Dashboard() {
-    return (
-        <div>
-            
-        </div>
-    )
+  return (
+    <Container>
+      <Row>
+        <Col className="bg-light border">.col</Col>
+      </Row>
+      <Row>
+        <Col className="bg-light border">.col</Col>
+        <Col className="bg-light border">.col</Col>
+        <Col className="bg-light border">.col</Col>
+        <Col className="bg-light border">.col</Col>
+      </Row>
+      <Row>
+        <Col className="bg-light border" xs="3">
+          .col-3
+        </Col>
+        <Col className="bg-light border" xs="auto">
+          .col-auto - variable width content
+        </Col>
+        <Col className="bg-light border" xs="3">
+          .col-3
+        </Col>
+      </Row>
+      <Row>
+        <Col className="bg-light border" xs="6">
+          .col-6
+        </Col>
+        <Col className="bg-light border" xs="6">
+          .col-6
+        </Col>
+      </Row>
+      <Row>
+        <Col className="bg-light border" sm="4" xs="6">
+          .col-6 .col-sm-4
+        </Col>
+        <Col className="bg-light border" sm="4" xs="6">
+          .col-6 .col-sm-4
+        </Col>
+        <Col className="bg-light border" sm="4">
+          .col-sm-4
+        </Col>
+      </Row>
+      <Row>
+        <Col
+          className="bg-light border"
+          sm={{
+            offset: 1,
+            order: 2,
+            size: 6,
+          }}
+        >
+          .col-sm-6 .order-sm-2 .offset-sm-1
+        </Col>
+      </Row>
+      <Row>
+        <Col
+          className="bg-light border"
+          md={{
+            offset: 3,
+            size: 6,
+          }}
+          sm="12"
+        >
+          .col-sm-12 .col-md-6 .offset-md-3
+        </Col>
+      </Row>
+      <Row>
+        <Col
+          className="bg-light border"
+          sm={{
+            offset: 1,
+            size: "auto",
+          }}
+        >
+          .col-sm-auto .offset-sm-1
+        </Col>
+        <Col
+          className="bg-light border"
+          sm={{
+            offset: 1,
+            size: "auto",
+          }}
+        >
+          .col-sm-auto .offset-sm-1
+        </Col>
+      </Row>
+    </Container>
+  );
 }
 
-export default Dashboard
+export default Dashboard;
