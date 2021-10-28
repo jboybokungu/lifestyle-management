41427bb765944bf2905eb38fb36ad459f2c999b4
Mel Vint <melvint@Mels-MacBook-Pro.local>
2021-10-28 13:37:01 -0400

    added spinner to homepage

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -9,62 +9,61 @@ import {
   CardText,
 } from "reactstrap";
 
-import GoalForm from '../components/GoalForm';
+import GoalForm from "../components/GoalForm";
 function Dashboard() {
-    return (
-     <div>
+  return (
+    <div>
       <GoalForm type="fitness" />
-        <CardGroup>
-         <Card>
+      <CardGroup>
+        <Card>
           <CardImg
-          alt="Sleep"
-          src="https://images.unsplash.com/photo-1517912172058-994f5e342dca?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=774&q=80"
-          top
-          width="100%"
-        />
-        <CardBody>
-          <CardTitle tag="h5"></CardTitle>
-          <CardSubtitle className="mb-2 text-muted" tag="h6"></CardSubtitle>
-          <CardText className="text-center">SLEEP</CardText>
-          {/* <Button>
+            alt="Sleep"
+            src="https://images.unsplash.com/photo-1517912172058-994f5e342dca?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=774&q=80"
+            top
+            width="100%"
+          />
+          <CardBody>
+            <CardTitle tag="h5"></CardTitle>
+            <CardSubtitle className="mb-2 text-muted" tag="h6"></CardSubtitle>
+            <CardText className="text-center"></CardText>
+            {/* <Button>
         Button
       </Button> */}
-        </CardBody>
-      </Card>
-      <Card>
-        <CardImg
-          alt="Fitness"
-          src="https://images.unsplash.com/photo-1610543123792-135b26601797?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=774&q=80"
-          top
-          width="100%"
-        />
-        <CardBody>
-          <CardTitle tag="h5"></CardTitle>
-          <CardSubtitle className="mb-2 text-muted" tag="h6"></CardSubtitle>
-          <CardText className="text-center"> FITNESS</CardText>
-          {/* <Button>
+          </CardBody>
+        </Card>
+        <Card>
+          <CardImg
+            alt="Fitness"
+            src="https://images.unsplash.com/photo-1610543123792-135b26601797?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=774&q=80"
+            top
+            width="100%"
+          />
+          <CardBody>
+            <CardTitle tag="h5"></CardTitle>
+            <CardSubtitle className="mb-2 text-muted" tag="h6"></CardSubtitle>
+            <CardText className="text-center"></CardText>
+            {/* <Button>
         Button
       </Button> */}
-        </CardBody>
-      </Card>
-      <Card>
-        <CardImg
-          alt="Diet"
-          src="https://images.unsplash.com/photo-1623855244697-5d8fbe9c7892?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=774&q=80"
-          top
-          width="100%"
-        />
-        <CardBody>
-          <CardTitle tag="h5"></CardTitle>
-          <CardSubtitle className="mb-2 text-muted" tag="h6"></CardSubtitle>
-          <CardText className="text-center">FOOD</CardText>
-          {/* <Button>
-        Button
-      </Button> */}
-        </CardBody>
-      </Card>
-    </CardGroup>
-      </div>
+          </CardBody>
+        </Card>
+        <Card>
+          <CardImg
+            alt="Diet"
+            src="https://images.unsplash.com/photo-1623855244697-5d8fbe9c7892?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=774&q=80"
+            top
+            width="100%"
+          />
+          <CardBody>
+            <CardTitle tag="h5"></CardTitle>
+            <CardSubtitle className="mb-2 text-muted" tag="h6"></CardSubtitle>
+            <CardText className="text-center"></CardText>
+          </CardBody>
+        </Card>
+      </CardGroup>
+  </div>
+
+  
   );
 }
 
