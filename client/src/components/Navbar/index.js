bee9dac27278d4ec77ef916c1f5a0ab010b6f0cd
Lajaynees <lscingram@yahoo.com>
2021-10-28 13:37:25 -0400

    correct all err in Nav-adding icons

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -26,37 +26,32 @@ const Navbar = () => {
 
   return (
     <>
-     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-  <Link className="navbar-brand" to="/">L.Y.B.L</Link>
-  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span className="navbar-toggler-icon"></span>
-  </button>
-
-  <div className="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul className="navbar-nav m-auto">
-      <li className="nav-item active">
-        {/* <Link className="nav-link" to="/"Dashboard>className="sr-only"></Link> */}
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <Link className="navbar-brand" to="/">L.Y.B.L</Link>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+          <Link className="dropdown-item" to="/dashboard">Dashboard</Link>
+          <Link className="dropdown-item" to="/me">Profile</Link>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav m-auto">
+          <li class="nav-item active">
+        <Link class="nav-link" to="/me">Profile <span class="navbar-toggler-icon"></span></Link>
+        
       </li>
       <li className="nav-item">
-        <Link className="nav-link" to="/">Log-Out</Link>
-      </li>
-      <li className="nav-item dropdown">
-        <Link className="nav-link dropdown-toggle" to="/" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Dropdown
-        </Link>
-        <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-          <Link className="dropdown-item" to="/">Profile</Link>
-          <Link className="dropdown-item" to="/">Goals</Link>
-          <Link className="dropdown-item" to="/Signup">Logout</Link>
+              <Link className="nav-link" to="/">Log-Out</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/dashboard">Dashboard</Link>
+            </li><li className="nav-item dropdown">
+             
+              
+            </li>
+          </ul>
 
-          {/* <div className="dropdown-divider"></div>
-          <a className="dropdown-item" href="#">Something else here</a> */}
         </div>
-      </li>
-    </ul>
-  
-  </div>
-</nav> 
+      </nav>
     </>
   );
 };
