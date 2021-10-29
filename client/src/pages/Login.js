05ac821828a5cdf04ca8a20e76a367dea53d9abf
Lajaynees <lscingram@yahoo.com>
2021-10-29 11:36:38 -0400

    updated footer-styled login/logout

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -2,8 +2,8 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useMutation } from '@apollo/client';
 import { LOGIN_USER } from '../utils/mutations';
-
 import Auth from '../utils/auth';
+import { Col, Row, Form, FormGroup, Label, Button } from "reactstrap";
 
 const Login = () => {
   const [formState, setFormState] = useState({ email: '', password: '' });
@@ -47,39 +47,54 @@ const Login = () => {
           <Link to="/">back to the homepage.</Link>
         </p>
       )
-    } 
+    }
     return (
-      <form onSubmit={handleFormSubmit}>
-        <input
-          placeholder="Email"
-          name="email"
-          type="email"
-          value={formState.email}
-          onChange={handleChange}
-        />
-        <input
-          placeholder="******"
-          name="password"
-          type="password"
-          value={formState.password}
-          onChange={handleChange}
-        />
-        <button type="submit">
-          Submit
-        </button>
-      </form>
+      <form className="text-center" onSubmit={handleFormSubmit}>
+        <Form>
+          <Row xs="3">
+            <Col md={6}>
+              <FormGroup>
+                <Label for="Email"></Label>
+                <input
+                  className="email text-success text-center"
+                  placeholder="Email"
+                  name="email"
+                  type="email"
+                  value={formState.email}
+                  onChange={handleChange}
+                />
+              </FormGroup>
+              <FormGroup>
+                <Label for="Password"></Label>
+                <input
+                  className="password text-center"
+                  placeholder="******"
+                  name="password"
+                  type="password"
+                  value={formState.password}
+                  onChange={handleChange}
+                />
+              </FormGroup>
+              <Button outline color="secondary" type="submit">
+                Submit
+              </Button>{" "}
+            </Col>
+          </Row>
+        </Form>
+      </form >
     );
   };
 
-  return (
-    <main>
-      <h4>Login</h4>
-      <div>
-        {renderForm()}
-        {error && <div>{error.message}</div>}
-      </div>
-    </main>
-  );
+
+return (
+  <>
+    <h4>Login</h4>
+    <div>
+      {renderForm()}
+      {error && <div>{error.message}</div>}
+    </div>
+  </>
+);
 };
 
 export default Login;
