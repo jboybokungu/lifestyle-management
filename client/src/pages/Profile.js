1b19d9406e19404b7a982399f63623a84cae1cbf
Mel Vint <melvint@Mels-MacBook-Pro.local>
2021-10-30 00:18:07 -0400

    styled

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -6,7 +6,7 @@ import { useQuery } from '@apollo/client';
 import Auth from '../utils/auth';
 import { QUERY_USER, QUERY_ME } from '../utils/queries';
 // Components
-import UserList from '../components/UserList';
+
 
 const Profile = () => {
   const { id } = useParams();
